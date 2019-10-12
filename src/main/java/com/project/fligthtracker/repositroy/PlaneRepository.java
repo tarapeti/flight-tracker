a5b524ec7646f547f920af5c3fb7afab77a613d8
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-12 12:37:28 +0200

    created repository interface

--- src/main/java/com/project/fligthtracker/repositroy/PlaneRepository.java
+++ src/main/java/com/project/fligthtracker/repositroy/PlaneRepository.java
@@ -1,4 +1,9 @@
 package com.project.fligthtracker.repositroy;
 
-public class PlaneRepository {
+import com.project.fligthtracker.model.Plane;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaneRepository extends JpaRepository<Plane, Integer> {
 }
