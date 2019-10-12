79e914a67234ccbebce42c3aaeab06796ebbf00d
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-12 14:08:12 +0200

    tested and implemented interface save method

--- src/main/java/com/project/fligthtracker/repositroy/PlaneRepository.java
+++ src/main/java/com/project/fligthtracker/repositroy/PlaneRepository.java
@@ -1,9 +1,13 @@
 package com.project.fligthtracker.repositroy;
 
-import com.project.fligthtracker.model.Plane;
+import com.project.fligthtracker.model.Planes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlaneRepository extends JpaRepository<Plane, Integer> {
+public interface PlaneRepository extends JpaRepository<Planes, Integer> {
+    @Override
+    <S extends Planes> S save(S entity);
+
+    Planes findByCompanyName(String companyName);
 }
--- src/test/java/com/project/fligthtracker/FligthtrackerApplicationTests.java
+++ src/test/java/com/project/fligthtracker/FligthtrackerApplicationTests.java
@@ -1,16 +1,36 @@
 package com.project.fligthtracker;
 
+import com.project.fligthtracker.model.Planes;
+import com.project.fligthtracker.repositroy.PlaneRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FligthtrackerApplicationTests {
 
+	@Autowired
+	private PlaneRepository planeRepository;
+
 	@Test
-	public void contextLoads() {
+	public void whenSaved_thenFindsByName() {
+		//given
+		Planes plane = new Planes("QUATAR", "Miskolc", "Pest", 0, 1 ,0);
+
+		planeRepository.save(plane);
+
+		//when
+		Planes foundPlane = planeRepository.findByCompanyName(plane.getCompanyName());
+
+		//then
+		assertEquals(plane.getCompanyName(), foundPlane.getCompanyName());
 	}
+
 
 }
