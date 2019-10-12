ef5d429dcca95fb52bb8c5bf165733ad60aa8bfc
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-12 15:07:32 +0200

    implemented service methods from repository, implemented update

--- src/main/java/com/project/fligthtracker/service/PlaneService.java
+++ src/main/java/com/project/fligthtracker/service/PlaneService.java
@@ -1,4 +1,18 @@
 package com.project.fligthtracker.service;
 
+import com.project.fligthtracker.model.Planes;
+
+import java.util.List;
+
 public interface PlaneService {
+
+    void savePlane(Planes plane);
+
+    void deletePlane(Planes plane);
+
+    List<Planes> findAllPlanes();
+
+    Planes findPlaneByCompanyName(String companyName);
+
+    void updatePlane(Planes planes);
 }
--- src/main/java/com/project/fligthtracker/service/simple/SimplePlaneService.java
+++ src/main/java/com/project/fligthtracker/service/simple/SimplePlaneService.java
@@ -1,6 +1,39 @@
 package com.project.fligthtracker.service.simple;
 
+import com.project.fligthtracker.model.Planes;
+import com.project.fligthtracker.repositroy.PlaneRepository;
 import com.project.fligthtracker.service.PlaneService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class SimplePlaneService implements PlaneService {
+    @Autowired
+    private PlaneRepository planeRepository;
+
+    @Override
+    public void savePlane(Planes plane) {
+        planeRepository.save(plane);
+    }
+
+    @Override
+    public void deletePlane(Planes plane) {
+        planeRepository.delete(plane);
+    }
+
+    @Override
+    public List<Planes> findAllPlanes() {
+        return planeRepository.findAll();
+    }
+
+    @Override
+    public Planes findPlaneByCompanyName(String companyName) {
+        return planeRepository.findByCompanyName(companyName);
+    }
+
+    @Override
+    public void updatePlane(Planes plane) {
+        //jpa repositroy's save works as update if the databas already contains the objects id in it
+        planeRepository.save(plane);
+    }
 }
