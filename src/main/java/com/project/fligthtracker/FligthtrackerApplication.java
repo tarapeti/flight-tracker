a4fc1f0b5c38b6268bd6ac69f1adc0b533599afe
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-12 11:57:04 +0200

    basic skeleton created via Spring Initializr

---
+++ src/main/java/com/project/fligthtracker/FligthtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.fligthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FligthtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FligthtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fligthtracker/FligthtrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.fligthtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FligthtrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
