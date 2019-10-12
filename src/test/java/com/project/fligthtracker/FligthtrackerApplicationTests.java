0da5748e46c62284b3ea3526cbed4e0aab03852d
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-12 15:09:54 +0200

    injected service class, tested update

--- src/test/java/com/project/fligthtracker/FligthtrackerApplicationTests.java
+++ src/test/java/com/project/fligthtracker/FligthtrackerApplicationTests.java
@@ -2,10 +2,14 @@ package com.project.fligthtracker;
 
 import com.project.fligthtracker.model.Planes;
 import com.project.fligthtracker.repositroy.PlaneRepository;
+import com.project.fligthtracker.service.PlaneService;
+import com.project.fligthtracker.service.simple.SimplePlaneService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -20,6 +24,19 @@ public class FligthtrackerApplicationTests {
 
 	@Autowired
 	private PlaneRepository planeRepository;
+
+	@TestConfiguration
+	static class PlaneServiceImplTestContextConfiguration {
+
+		@Bean
+		public PlaneService planeService() {
+			return new SimplePlaneService();
+		}
+	}
+
+	@Autowired
+	private PlaneService planeService;
+
 
 	@Test
 	public void whenSaved_thenFindsByName() {
@@ -55,7 +72,7 @@ public class FligthtrackerApplicationTests {
 
 	@Test
 	public void whenDeleted_doesntFind(){
-		//gien
+		//given
 		Planes plane = new Planes("QUATAR", "Miskolc", "Pest", 0, 1 ,0);
 		planeRepository.save(plane);
 
@@ -64,8 +81,23 @@ public class FligthtrackerApplicationTests {
 
 		//then
 		assertNull(planeRepository.findByCompanyName(plane.getCompanyName()));
-
 	}
 
+	@Test
+	public void whenUpdated_thenChanges(){
+		//given
+		Planes plane = new Planes("QUATAR", "Miskolcasd", "Pest", 0, 1 ,0);
+		planeRepository.save(plane);
+		String newDeparturePlace = "Pécs";
+		plane.setDeparturePlace(newDeparturePlace);
+
+
+		//when
+		planeService.updatePlane(plane);
+
+		//then
+		assertEquals(planeRepository.findByCompanyName(plane.getCompanyName()).getDeparturePlace(), newDeparturePlace);
+
+	}
 
 }
