bb7c915d0cfc58b46830d3aa15133bcdbf0c82be
Taraszovics Péter <gtarapeti@gmail.com>
2019-10-13 19:39:48 +0200

    getters setters def constructor

--- src/main/java/com/project/fligthtracker/dto/PlaneInfoToTableDto.java
+++ src/main/java/com/project/fligthtracker/dto/PlaneInfoToTableDto.java
@@ -4,9 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
 public class PlaneInfoToTableDto {
 
         private int id;
@@ -32,6 +29,64 @@ public class PlaneInfoToTableDto {
         this.landingPlace = landingPlace;
         this.departureTimeInMillis = departureTimeInMillis;
         this.landingTimeInMillis = landingTimeInMillis;
+        this.delay = delay;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDeparturePlace() {
+        return departurePlace;
+    }
+
+    public void setDeparturePlace(String departurePlace) {
+        this.departurePlace = departurePlace;
+    }
+
+    public String getLandingPlace() {
+        return landingPlace;
+    }
+
+    public void setLandingPlace(String landingPlace) {
+        this.landingPlace = landingPlace;
+    }
+
+    public String getDepartureTimeInMillis() {
+        return departureTimeInMillis;
+    }
+
+    public void setDepartureTimeInMillis(String departureTimeInMillis) {
+        this.departureTimeInMillis = departureTimeInMillis;
+    }
+
+    public String getLandingTimeInMillis() {
+        return landingTimeInMillis;
+    }
+
+    public void setLandingTimeInMillis(String landingTimeInMillis) {
+        this.landingTimeInMillis = landingTimeInMillis;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
         this.delay = delay;
     }
 }
--- src/main/java/com/project/fligthtracker/model/Planes.java
+++ src/main/java/com/project/fligthtracker/model/Planes.java
@@ -8,9 +8,6 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "planes")
-@Getter
-@Setter
-@NoArgsConstructor
 public class Planes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,6 +31,66 @@ public class Planes {
         this.landingPlace = landingPlace;
         this.departureTimeInMillis = departureTimeInMillis;
         this.landingTimeInMillis = landingTimeInMillis;
+        this.delay = delay;
+    }
+
+    public Planes() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDeparturePlace() {
+        return departurePlace;
+    }
+
+    public void setDeparturePlace(String departurePlace) {
+        this.departurePlace = departurePlace;
+    }
+
+    public String getLandingPlace() {
+        return landingPlace;
+    }
+
+    public void setLandingPlace(String landingPlace) {
+        this.landingPlace = landingPlace;
+    }
+
+    public long getDepartureTimeInMillis() {
+        return departureTimeInMillis;
+    }
+
+    public void setDepartureTimeInMillis(long departureTimeInMillis) {
+        this.departureTimeInMillis = departureTimeInMillis;
+    }
+
+    public long getLandingTimeInMillis() {
+        return landingTimeInMillis;
+    }
+
+    public void setLandingTimeInMillis(long landingTimeInMillis) {
+        this.landingTimeInMillis = landingTimeInMillis;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
         this.delay = delay;
     }
 }
